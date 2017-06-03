27c91943ab9fd7b76c8d866ff9a79e55104dc87a
Patrick Gillan <patrick@minorimpact.com>
2017-06-03 16:34:23 +0000

    Minor fix for user database.

--- MinorImpact.pm
+++ MinorImpact.pm
@@ -53,8 +53,8 @@ sub new {
         $self->{DB} = DBI->connect($dsn, $config->{db}->{db_user}, $config->{db}->{db_password}, {RaiseError=>1, mysql_auto_reconnect=>1}) || die("Can't connect to database");
 
         if ($confg->{user_db}) {
-            my $dsn = "DBI:mysql:database=$config->{db}->{database};host=$config->{db}->{db_host};port=$config->{db}->{port}";
-            $self->{USERDB} = DBI->connect($dsn, $config->{db}->{db_user}, $config->{db}->{db_password}, {RaiseError=>1, mysql_auto_reconnect=>1}) || die("Can't connect to database");
+            my $dsn = "DBI:mysql:database=$config->{user_db}->{database};host=$config->{user_db}->{db_host};port=$config->{user_db}->{port}";
+            $self->{USERDB} = DBI->connect($dsn, $config->{user_db}->{db_user}, $config->{user_db}->{db_password}, {RaiseError=>1, mysql_auto_reconnect=>1}) || die("Can't connect to database");
         } else {
             $self->{USERDB} = $self->{DB};
         }
